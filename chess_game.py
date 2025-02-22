c8d425ee01c826ee63ec4be8766813688a88f17e
FUYONG <x123xx777+git@gmail.com>
2025-02-22 17:14:36 +0800

    完成易位規則; 完成部分遊戲進行的流程; 修改小兵初次移動失敗後, 無法一次往前兩格的bug; 修改棋盤印出過程; 增加使用者選擇其操作系統

--- chess_game.py
+++ chess_game.py
@@ -9,7 +9,6 @@ class Board:
     def __init__(self):
         self.board = [[" " for i in range(self.BOARD_SIZE)] for j in range(self.BOARD_SIZE)]
 
-        
         for i in range(self.BOARD_SIZE):
             for j in range(self.BOARD_SIZE):
                 # 設定棋盤座標
@@ -27,7 +26,7 @@ class Board:
     # 印出棋盤 
     def print_board(self):
         print("-"*(6*self.BOARD_SIZE))
-        print(f"{"CHESS GAME":^54s}")
+        print(f"{'ChessGeme':^54s}")
         for i in range(self.BOARD_SIZE):
             print("-"*(6*self.BOARD_SIZE))
             for j in range(self.BOARD_SIZE):
@@ -38,8 +37,8 @@ class Board:
     
             print()
 
-    # 下棋
-    def draw(self, currentPosition, nextPosition):
+    # 移動棋子
+    def moveChess(self, currentPosition, nextPosition, playerGroup):
         # 轉換座標
         currentX = self.__standardPosition(currentPosition[0])
         currentY = self.__standardPosition(currentPosition[1])
@@ -47,19 +46,66 @@ class Board:
         nextX = self.__standardPosition(nextPosition[0])
         nextY = self.__standardPosition(nextPosition[1])
 
+        chessKind = self.board[currentY][currentX]  # 取得棋子類型
+
+        # 檢查玩家移動的是自己的棋子 
+        if type(chessKind) != str and chessKind.group != playerGroup:
+            return False
         
+        # 檢查是否符合易位的規則
+        if type(chessKind) == King and abs(currentX - nextX) == 2 and self.__checkCastling(currentX, currentY, nextX, nextY):
+            # 找出城堡的位置
+            x = (nextX - currentX)//abs(nextX - currentX) 
+            y = currentY - nextY
+
+            rookCurrentX = currentX 
+            rookCurrentY = currentY
+
+            while 1 <= rookCurrentX <= 8 and 0 <= rookCurrentY <= 7:
+
+                if type(self.board[rookCurrentY][rookCurrentX]) == Rook:
+                    break
+                else:
+                    rookCurrentX += x
+                    rookCurrentY += y 
+
+            
+            # 易位
+            self.__draw(currentX, currentY, nextX, nextY)
+            self.__draw(rookCurrentX, rookCurrentY, nextX - x, nextY)
+
+            # 將棋子設定成已移動過
+            chessKind.setEverMove()
+            self.board[nextY][nextX - x].setEverMove() # 城堡的位置
+            
+            # 建立攻擊表
+            self.__buildAttackList(chessKind.group)
+
+            return True
         
-        # check move and draw
-        if self.__checkMoveRule(currentX, currentY, nextX, nextY):
-            chessKind = self.board[currentY][currentX]  # 取得棋子類型
-            self.board[currentY][currentX] = " "        # 清空原本位置
-            self.board[nextY][nextX] = chessKind        # 移動到新位置
+        # 檢查是否吃過路兵
+        if type(chessKind) == Pawn:
+            pass
+
+        # 檢查是否符合移動規則
+        if self.__checkMoveRule(currentX, currentY, nextX, nextY):     
+            self.__draw(currentX, currentY, nextX, nextY)
+            
+            # 將棋子設定成已移動過
+            chessKind.setEverMove() 
 
             # 建立攻擊表
             self.__buildAttackList(chessKind.group)
+
             return True
         else:
             return False
+
+    # 下棋
+    def __draw(self, currentX, currentY, nextX, nextY):  
+        chessKind = self.board[currentY][currentX]  # 取得棋子
+        self.board[currentY][currentX] = " "        # 清空原本位置
+        self.board[nextY][nextX] = chessKind        # 移動到新位置
 
     # 將字元轉成正確的數字格式
     def __standardPosition(self, position):
@@ -123,7 +169,8 @@ class Board:
         # 把小兵吃子與移動的規則區隔開來 
         if type(chessKind) == Pawn:
             checkMove = checkMove and type(targetLocation) == str
-        
+            
+
         # 騎士不受中間有其他棋子擋住的限制 
         if type(chessKind) != Knight:
             checkEat = checkEat and checkBlock
@@ -134,7 +181,7 @@ class Board:
             self.__checkPromotion(currentX, currentY, nextX, nextY)
 
         # 檢查是否符合規則
-        if checkMove or checkEat: 
+        if checkMove or checkEat:               
             return True
         else:
             return False
@@ -231,7 +278,6 @@ class Board:
                             self.attackList[y][x] = "X"
 
                         elif targetLocation.group != group:
-                            print("test")
                             self.attackList[y][x] = "X"
                             break
                         else:
@@ -256,12 +302,84 @@ class Board:
                     print(f"{self.attackList[i][j]:^5s}", end = "|")
             
             print()
+    
+    # 檢查王車易位
+    def __checkCastling(self, currentX, currentY, nextX, nextY):
+
+        chessKind = self.board[currentY][currentX] # 記錄國王位置
+        rookLocation = None # 記錄城堡的位置
+         
+        noAttack = True # 記錄移動路徑是否受到攻擊
+        noChess = True # 記錄中間是否有其他棋子
+
+        # 記錄移動的方向
+        x = (nextX - currentX)//abs(nextX - currentX) 
+        y = currentY - nextY
+
+        pointX = currentX # 記錄目前的位置
+        pointY = currentY # 記錄目前的位置
+
+        # y軸不能移動
+        if y != 0:
+            return False
+
+        # 檢查各項條件
+        while 1 <= pointX <= 8 and 0 <= pointY <= 7:
+            currentLocation = self.board[pointY][pointX]
+
+            # 找到城堡
+            if type(currentLocation) == Rook:
+                rookLocation = self.board[pointY][pointX]
+                break # 找到城堡就退出
+            
+            # 確認中間沒有其他棋子
+            if type(currentLocation) != str and type(currentLocation) != Rook and type(currentLocation) != King:
+                print(f"中間有其他棋子")
+                noChess = False
+
+            # 確認國王的移動範圍未受到攻擊
+            if abs(currentX - pointX) <= 2 and self.attackList[pointY][pointX] == "X":
+                self.printAttackList()
+                print(f"受到攻擊")
+                noAttack = False
+
+            pointX += x
+
+            # 超出範圍
+            if (1 <= pointX <= 8 and 0 <= pointY <= 7) == False:
+                print(f"超出範圍")
+                if(rookLocation == None):
+                    print(f"找不到城堡")
+                    return False
+                
+                break
             
 
+        # 確認城堡與國王未移動過
+        everMoveRook = rookLocation.getEverMove()
+        everMoveKing = chessKind.getEverMove()
+
+        # 確認是否符合易位的規則
+        if not(everMoveRook or everMoveKing) and noChess and noAttack and rookLocation != None:
+            # 將國王與城堡設定成已被移動過
+            rookLocation.setEverMove()
+            chessKind.setEverMove()
+            return True
+        else:
+            return False
+
+
 class Chess:
-    def __init__(self, kind, group):
+    __evenMove = False
+    def __init__(self, kind, group): 
         self.kind = kind
         self.group = group
+    
+    def setEverMove(self):
+        self.__evenMove = True
+    
+    def getEverMove(self):
+        return self.__evenMove
 
 
 class King(Chess):
@@ -337,15 +455,12 @@ class Rook(Chess):
 
 
 class Pawn(Chess):
-    firstMove = True
-
     def checkMove(self, x1, y1, x2, y2):
         x = x2 - x1
         y = y2 - y1
 
-        if self.firstMove:
+        if not(self.getEverMove()):
             if((y <= 2 and self.group == "black") or (y >= -2 and self.group == "white")) and x == 0:
-                self.firstMove = False
                 return True
             else:
                 return False
@@ -360,7 +475,6 @@ class Pawn(Chess):
         y = y2 - y1
 
         if((y == 1 and self.group == "black") or (y == -1 and self.group == "white")) and (x == 1 or x == -1):
-            self.firstMove = False
             return True
         else:
             return False
@@ -370,44 +484,84 @@ class Pawn(Chess):
             return [(-1, -1), (1, -1)]
         elif self.group == "black":
             return [(-1, 1), (1, 1)]
+    
+    def setFistMove(self):
+        self.__firstMove = False
+    
+    def getFirstMove(self):
+        return self.__firstMove
 
 
 class ChessGame:
+    __usersystem = "windows"
+    __clearprompt = None
+
     def __init__(self):
         self.chessBoard = Board()
+        self.setUserSystem()
+        
 
     def start(self):
+
+        # 簡單的交互
+        currentPlayer = "white"
+        switchPlayer = {
+            "black" : "white",
+            "white" : "black"
+        }
+
+        os.system(self.__clearprompt) # 清空畫面
+
+        self.chessBoard.print_board()
+
         while(True):
             try:
-                self.chessBoard.print_board()
-                print("------- 分隔線 -------")
-                self.chessBoard.printAttackList()
-
-                control = input("please input position: ").split(" ")
                 
+
+                control = input(f"Your move, {currentPlayer}, please input position: ").split(" ")
+                
+                # 檢查是否符合輸入格式
                 if(len(control)!= 2 and control[0] != "q"):
                     raise Exception("Input Error")
                 elif(control[0] == "q"):
                     print("gameover")
                     break
-                else:
-                    os.system("clear")
-                    if self.chessBoard.draw(control[0], control[1]) == False:
-                        os.system("clear")
-                        print("Can't Move")
+                
+                os.system(self.__clearprompt)
 
-                print(control)
+                if self.chessBoard.moveChess(control[0], control[1], currentPlayer):
+                    self.chessBoard.print_board()
+                    currentPlayer = switchPlayer[currentPlayer]
+                else:
+                    self.chessBoard.print_board()
+                    print("Can't Move")
+
             except Exception as e:
                 print(f"Error: {e}")
                 control = input("continue?: ")
                 if control == "q":
                     break
                 else:
-                    os.system("clear")
+                    os.system(self.__clearprompt)
             else:
                 pass
                 
-
+    def setUserSystem(self):
+        while True:
+            try:
+                self.__usersystem = str(input("input your OS(windows/linux/mac): "))
+                if(self.__usersystem not in ["windows", "linux", "mac"]):
+                    raise Exception("Input Error")
+                else:
+                    if self.__usersystem == "windows":
+                        self.__clearprompt = "cls"
+                    
+                    if self.__usersystem in ["linux", "mac"]:
+                        self.__clearprompt = "clear"
+                    break
+            except Exception as e:
+                print(f"error: {e}")
+              
 if __name__ == "__main__":
     chessGame = ChessGame()
     chessGame.start()
